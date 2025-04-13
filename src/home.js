b92d119eeb25e0776af047170a864a4368ca46d5
cinekfarmer <cinekfarmer@gmail.com>
2025-04-13 09:49:23 +0200

    Add working navigation
    Now, when user clicks on navigation buttons, they will change their colour. Function will display the content chosen by clicked button.

---
+++ src/home.js
@@ -0,0 +1,8 @@
+export default `
+        <h2 class="content-title">Home</h2>
+        <p>
+            Welcome in Bread & Coffee! Here you can find rich offer of highest 
+            quality products like bread, coffee and water. Please, don't be shie. 
+            Just look what we can offer you. Enjoy!
+        </p>
+`;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import "./style.css";
+
+import homeHTML from "./home.js";
+import menuHTML from "./menu.js";
+import aboutHTML from "./about.js";
+
+const HOME_BTN = document.querySelector('#homeBtn');
+
+const MENU_BTN = document.querySelector('#menuBtn');
+
+const ABOUT_BTN = document.querySelector('#aboutBtn');
+
+let lastActiveBtn;
+const start = () => {
+    lastActiveBtn = HOME_BTN;
+    activateButton(HOME_BTN);
+    generateContent(homeHTML);
+};
+
+HOME_BTN.addEventListener('click', () => {
+    activateButton(HOME_BTN);
+    generateContent(homeHTML);
+});
+MENU_BTN.addEventListener('click', () => {
+    activateButton(MENU_BTN);
+    generateContent(menuHTML);
+});
+ABOUT_BTN.addEventListener('click', () => {
+    activateButton(ABOUT_BTN);
+    generateContent(aboutHTML);
+});
+
+start();
+
+function activateButton(button) {
+    lastActiveBtn.classList.remove('active');
+    button.classList.add('active');
+    lastActiveBtn = button;
+}
+
+function generateContent(content) {
+    document.querySelector('#container').innerHTML = content;
+}   
